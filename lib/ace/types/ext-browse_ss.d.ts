2e3653c4249ec153d3c627770c89715769825563
Hai Le <Hai.Le@bnpparibas-pf.dk>
2026-01-28 15:57:35 +0100

    add browse_library

--- lib/ace/types/ext-browse_ss.d.ts
+++ lib/ace/types/ext-browse_ss.d.ts
@@ -2,5 +2,6 @@ export type KeyboardEventListener = (ev: KeyboardEvent) => any;
 declare global {
     interface Window {
         _browseSsFilesListener: KeyboardEventListener;
+        _browseSsLibraryListener: KeyboardEventListener;
     }
 }
