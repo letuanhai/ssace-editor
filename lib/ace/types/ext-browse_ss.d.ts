0c7db8749f4f77b38c2b75e43d40dc641a320e42
Hai Le <Hai.Le@bnpparibas-pf.dk>
2026-01-29 09:12:39 +0100

    fix typing

--- lib/ace/types/ext-browse_ss.d.ts
+++ lib/ace/types/ext-browse_ss.d.ts
@@ -1,6 +1,7 @@
 export type KeyboardEventListener = (ev: KeyboardEvent) => any;
 declare global {
     interface Window {
+        appDMS: any,
         _browseSsFilesListener: KeyboardEventListener;
         _browseSsLibraryListener: KeyboardEventListener;
     }
