733ae9b81e440e5885a1f5d1ab5ad36d55bf84d3
Hai Le <Hai.Le@bnpparibas-pf.dk>
2026-01-31 13:46:41 +0100

    disable debug logging

--- lib/ace/types/ext-browse_ss.d.ts
+++ lib/ace/types/ext-browse_ss.d.ts
@@ -1,6 +1,8 @@
 export type KeyboardEventListener = (ev: KeyboardEvent) => any;
 declare global {
     interface Window {
+        _browseSs_DEBUG: Boolean?,
+        _browseSsDebugLog: Function,
         appDMS: any,
         _browseSsFilesListener: KeyboardEventListener;
         _browseSsLibraryListener: KeyboardEventListener;
